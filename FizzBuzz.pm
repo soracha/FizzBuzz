9fdbabad62457e419c0a27f50fcb3069cb3df0d2
Soracha Thepsatitsilp <fai@abctech-thailand.com>
2012-06-21 11:14:02 +0700

    modify fizzbuzz() for 15

--- FizzBuzz.pm
+++ FizzBuzz.pm
@@ -3,11 +3,15 @@ package FizzBuzz;
 sub fizzbuzz {
 	my $number = shift @_;
 
-	if ( $number % 3 == 0 ) {
+	if ( ( $number % 3 == 0 ) && ( $number % 5 == 0 ) ) 	{
+		return "FizzBuzz";
+	}
+
+	elsif ( $number % 3 == 0 ) 	{
 		return "Fizz";
 	}
 
-	elsif ( $number % 5 == 0 ) {
+	elsif ( $number % 5 == 0 ) 	{
 		return "Buzz";
 	}
 
